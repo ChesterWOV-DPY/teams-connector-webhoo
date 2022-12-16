052d631e68677ea9ccc4769cb4c1fabc713b1e31
ChesterWOV <99033489+ChesterWOV-DPY@users.noreply.github.com>
2022-12-16 13:26:08 +0000

    Initial website finished

---
+++ process.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processing...</title>
+</head>
+<body>
+    <script>
+        const req = new XMLHttpRequest()
+        req.open('POST', "<?= $_POST['webhookUrl']; ?>");
+        req.addEventListener('load', () => {
+            window.location.replace("/");
+        });
+        req.send({
+            @type: 'MessageCard',
+            @context: 'https://schema.org/extensions',
+            text: "<?= $_POST['text'] ?>",
+            summary: "<?= $_POST['summary'] ?>",
+            title: "<?= $_POST['title'] ?>"
+        });
+    </script>
+</body>
+</html>
